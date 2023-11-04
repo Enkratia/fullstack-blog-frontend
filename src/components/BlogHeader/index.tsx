9a5d91bde308e1114aa4c4c0e5bf5ee4862b14c3
Enkratia <manofthetreasure@gmail.com>
2023-11-04 22:24:15 +0300

    m

--- src/components/BlogHeader/index.tsx
+++ src/components/BlogHeader/index.tsx
@@ -17,7 +17,7 @@ const post = {
   updatedAt: "2023-11-03T17:44:30.644Z",
   text: "Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident.",
   imageUrl:
-    "https://s3-alpha-sig.figma.com/img/6374/8d1f/bd83d188028be8a810fd08204554ab09?Expires=1699833600&Signature=iK4SYeEbawVrKZlui6vWqApdqDRVjlMbndteIy9MhuXLNGhsQE1xlFRe600xMQcxOzcb506k5-6ycex8Q~DyTfHSx7Lxqmr5hQ2dxoXqQnFW339XxGBvukpL8VcVM72WpdouW022a9yCgpZ0kDtFoc~i6-fLeXL92TqN8JQLQfB33JwpX9~-7NxW72R4VBDFQhSdmGleW~EhREuf7R-1scDwBrfENqtTv3UTahsOoQ4ekbXoX0tgUUIIEJ9lIdhraSrH2snttB47zV~Rsr8dx8tAS9uON5ralEYTJ0HpzrJO1X9ZZ-UE6LIiyEjfK-ErEyz0DgAQYx3y0b8qJnKaQQ__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
+    "https://s3-alpha-sig.figma.com/img/ffdd/fb94/98174abefa2a086436f620be49d2f85c?Expires=1699833600&Signature=if1TexCLIeTWV6k8yHI9J7UGHThFUEqezGPuqYrKkauuPXzP30TN1sJ88TAWzXCLHL2MOEO3TsDeaisJNF9aK2KuCVq-QU-iZJ5eiH7RmUJKOVGlcUS6o7H237xlExHVbThSxKaOcfXTuua-wyXQuwcuJR3JlZtJv9vJMWBT1APRXqA6pgZO0GIoMvTnTFbZY4R03RukOXnszQ2hPiS4HYBEr9vn3fueMsDMKME23TxhfWF~C9jzt9xv0nGKUV5y-9WDcinmHWj2KKgh1-vkGxYSm54QByiXjLMDPDlrhfN8ja~~9VSNIUkwe~J49o9vxcdxTslTA1d3ilgYWnqcrg__&Key-Pair-Id=APKAQ4GOSFWCVNEHN3O4",
   tags: ["business", "experience"],
   isFeatured: true,
   authorId: 1,
@@ -29,7 +29,7 @@ export const BlogHeader: React.FC = () => {
       <h2 className={cs.srOnly}>{`Featured post: ${post.title}`}</h2>
 
       <div className={`${s.container} ${cs.container}`}>
-        <div className={s.left}>
+        {/* <div className={s.left}>
           <span className={s.subtitle}>Featured post</span>
           <h3 className={`${s.title} ${cs.title}`}>{post.title}</h3>
 
@@ -45,20 +45,85 @@ export const BlogHeader: React.FC = () => {
 
           <p className={s.text}>{post.text}</p>
           <Link href={`/blog/${post.id}`} className={`${s.link} ${cs.btn}`}>{`Read More >`}</Link>
-        </div>
+        </div> */}
 
         <div className={s.right}>
           <div className={s.imageWrapper}>
-          <Image
-            src={post.imageUrl}
-            alt={post.title}
-            // width={515}
-            // height={359}
-            fill={true}
-            layout={'fill'}
-            objectFit={'cover'}
-            className={s.image}
-          />
+            {/* <Image
+              src={post.imageUrl}
+              alt={post.title}
+              width={1280}
+              height={515}
+              // fill
+              sizes="(min-width: 1260px) 515px, (min-width: 1000px) calc(91.25vw - 617px), calc(100vw - 684px)"
+              // layout="intrinsic"
+              className={s.image}
+            /> */}
+            {/* </div> */}
+
+            {/* <div style={{ position: "relative", maxWidth: "1280px", paddingTop: "56.25%" }}> */}
+            {/* <Image
+              src={post.imageUrl}
+              alt="Picture of the author"
+              // fill
+              // sizes="(min-width: 1280px) 1280px, (min-width: 1000px) calc(91.25vw - 617px), calc(100vw - 684px)"
+              // style={{
+              //   objectFit: "contain",
+              // }}
+            /> */}
+
+            {/* <img src={post.imageUrl} /> */}
+            {/* <Image
+              src={post.imageUrl}
+              alt="Picture of the author"
+              width={2250}
+              height={1500}
+              sizes="100vw"
+              // sizes="(min-width: 1280px) 1280px, (max-width: 1000px) calc(91.25vw - 300px)"
+              // sizes="50vw"
+              style={{
+                width: "100%",
+                height: "auto",
+                // className={s.image}
+              }}
+              // fill
+            /> */}
+
+            <Image
+              src={post.imageUrl}
+              alt="Picture of the author"
+              width={2250}
+              height={1500}
+              sizes="(max-width: 920px) 100vw, 33vw"
+              style={{
+                width: "100%",
+                height: "auto",
+              }}
+            />
+
+            <Image
+              src={post.imageUrl}
+              alt="Picture of the author"
+              width={2250}
+              height={1500}
+              sizes="(max-width: 920px) 100vw, 33vw"
+              style={{
+                width: "100%",
+                height: "auto",
+              }}
+            />
+
+            <Image
+              src={post.imageUrl}
+              alt="Picture of the author"
+              width={2250}
+              height={1500}
+              sizes="(max-width: 920px) 100vw, 33vw"
+              style={{
+                width: "100%",
+                height: "auto",
+              }}
+            />
           </div>
         </div>
       </div>
