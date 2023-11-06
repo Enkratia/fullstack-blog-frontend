d139778b977c340bc1da9eb0739147f36bafcae4
Enkratia <manofthetreasure@gmail.com>
2023-11-06 23:18:19 +0300

    m

--- next.config.js
+++ next.config.js
@@ -1,39 +1,7 @@
 /** @type {import('next').NextConfig} */
-// const nextConfig = {};
-
-// const isProd = process.env.NODE_ENV === "production";
-
-// module.exports = nextConfig;
-
-// module.exports = {
-// webpack(config) {
-//   config.module.rules.push({
-//     test: /\.svg$/,
-//     use: [{ loader: "@svgr/webpack", options: { icon: true } }],
-//   });
-//   return config;
-// },
-// images: {
-//   remotePatterns: [
-//     {
-//       protocol: "https",
-//       hostname: "fakestoreapi.com",
-//       port: "",
-//       pathname: "/img/**",
-//     },
-//     {
-//       protocol: "https",
-//       hostname: "img.freepik.com",
-//       port: "",
-//       pathname: "/free-vector/**",
-//     },
-//   ],
-// },
-// };
 
 const nextConfig = {
-  // Use the CDN in production and localhost for development.
-  // assetPrefix: isProd ? "https://сdn.incomparable-truffle-a06b40.netlify.app" : undefined,
+  output: "standalone",
   images: {
     remotePatterns: [
       {
@@ -43,14 +11,14 @@ const nextConfig = {
         pathname: "/img/**",
       },
     ],
-    remotePatterns: [
-      {
-        protocol: "https",
-        hostname: "vast-tam-lamb.cyclic.app",
-        port: "",
-        pathname: "/images/**",
-      },
-    ],
+    // remotePatterns: [
+    //   {
+    //     protocol: "https",
+    //     hostname: "vast-tam-lamb.cyclic.app",
+    //     port: "",
+    //     pathname: "/images/**",
+    //   },
+    // ],
   },
   webpack(config) {
     config.module.rules.push({
